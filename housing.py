4d3bce509e535ee2f7e7a014cf1edd59ef46ecef
bellumfatum3 <41601068+bellumfatum3@users.noreply.github.com>
2019-05-16 17:26:05 -0400

    Add files via upload
    pg 67 ish of Hands-On Machine Learning with Scikit-Learn and TensorFlow published by O'Reilly

---
+++ housing.py
@@ -0,0 +1,159 @@
+from pandas.plotting import scatter_matrix
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import hashlib
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import LinearRegression
+try:
+    from sklearn.compose import ColumnTransformer
+except ImportError:
+    from future_encoders import ColumnTransformer # Scikit-Learn < 0.20
+
+
+housing_path = "C:/Users/alinaqvim/Documents/ML/"
+def load_housing_data(housing_path=housing_path):
+ csv_path = os.path.join(housing_path, "housing.csv")
+ return pd.read_csv(csv_path)
+housing = load_housing_data()
+print(housing.describe())
+#housing.hist(bins=50, figsize=(20,15))
+#plt.show()
+
+def split_train_test(data, test_ratio):
+ shuffled_indices = np.random.permutation(len(data))
+ test_set_size = int(len(data) * test_ratio)
+ test_indices = shuffled_indices[:test_set_size]
+ train_indices = shuffled_indices[test_set_size:]
+ return data.iloc[train_indices], data.iloc[test_indices]
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
+
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+ strat_train_set = housing.loc[train_index]
+ strat_test_set = housing.loc[test_index]
+ 
+for set in (strat_train_set, strat_test_set):
+ set.drop(["income_cat"], axis=1, inplace=True)
+
+# Copy Training Set
+housing = strat_train_set.copy()
+# Plot Geographical Data. Should look like California. Alpha = .1 tells me where there is high density
+housing.plot(kind="scatter", x="longitude", y="latitude",alpha = .1)
+
+# Predefined color map 
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+ s=housing["population"]/100, label="population",
+ c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+)
+plt.legend()
+
+corr_matrix = housing.corr()
+
+df1 = corr_matrix["median_house_value"].sort_values(ascending=False)
+
+print(df1)
+# Plot a scatter matrix of given attributes
+attributes = ["median_house_value", "median_income", "total_rooms",
+ "housing_median_age"]
+
+# Plot a scatter matrix of just median_income against median_house_value
+scatter_matrix(housing[attributes], figsize=(12, 8))
+housing.plot(kind="scatter", x="median_income", y="median_house_value",
+ alpha=0.1)
+
+# Create a new attribute. total_bedrooms = total_bedrooms/total_rooms
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+corr_matrix = housing.corr()
+df2 = corr_matrix["median_house_value"].sort_values(ascending=False)
+print(df2)
+
+# fill any nulls/blanks with median. There is also fillna method but this is fancy
+imputer = Imputer(strategy="median")
+# drop non-numeric column
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer.fit(housing_num)
+# Create a simply Numpy Array from imputed values then bring it back to the Dataframe
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+# Label our dataset for a non_numerical column
+encoder = LabelEncoder()
+housing_cat = housing["ocean_proximity"]
+housing_cat_encoded = encoder.fit_transform(housing_cat)
+housing_cat_encoded
+
+print(housing_cat_encoded)
+
+# Another option to encode using one-hot vectors
+encoder = OneHotEncoder()
+housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1,1))
+housing_cat_1hot
+
+
+# Using what we learned:
+# Apply both transformations (from text categories to integer categories, then
+# from integer categories to one-hot vectors) in one shot using the LabelBinarizer
+# class:
+encoder = LabelBinarizer()
+housing_cat_1hot = encoder.fit_transform(housing_cat)
+housing_cat_1hot
+
+# Custom Transformers
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+ def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+   self.add_bedrooms_per_room = add_bedrooms_per_room
+ def fit(self, X, y=None):
+   return self # nothing else to do
+ def transform(self, X, y=None):
+   rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+   population_per_household = X[:, population_ix] / X[:, household_ix]
+   if self.add_bedrooms_per_room:
+     bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+     return np.c_[X, rooms_per_household, population_per_household,
+     bedrooms_per_room]
+   else:
+     return np.c_[X, rooms_per_household, population_per_household]
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+# Using Pipeline, transform data
+num_pipeline = Pipeline([
+ ('imputer', Imputer(strategy="median")),
+ ('attribs_adder', CombinedAttributesAdder()),
+ ('std_scaler', StandardScaler()),
+ ])
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+# Complete Pipeline. Previous is just a small scale example
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+
+housing_prepared = full_pipeline.fit_transform(housing)
